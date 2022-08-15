2483c1001c6c41d1aaf1844c616d8bc454f66d88
dieptv1999 <diep.tv199@gmail.com>
2022-08-15 18:42:22 +0700

    .

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -9,7 +9,7 @@ import { REDIS } from './redis.constants';
       provide: REDIS,
       useFactory: async () => {
         const client = Redis.createClient({
-          url: 'redis://localhost:6379',
+          url: 'redis://default:yvrV6y8XqwG5TfaAnn4Lj5QXoNsWKN9D@redis-11240.c252.ap-southeast-1-1.ec2.cloud.redislabs.com:11240',
           legacyMode: true,
         });
         await client.connect();
